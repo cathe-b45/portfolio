4fcd0c5a37f34b42d8880d266b6dc47077cdfdc7
Catherine Ballester <75802344+cathe-b45@users.noreply.github.com>
2026-01-09 23:08:59 +0100

    astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,5 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://cathe-b45.github.io",
-  base: "/",
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  base: "/portfolio/",
+  vite: { plugins: [tailwindcss()] },
 });
